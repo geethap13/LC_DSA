231ea5e90808c3e0d75b549d62041d4280ca5a33
Geetha <104586263+geethap13@users.noreply.github.com>
2023-03-27 13:06:23 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (18.91%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,6 +13,55 @@
  *     }
  * }
  */
+ /*
+     class Pair{
+        TreeNode root;
+        int depth;
+        Pair(TreeNode _root,int _depth){
+            root = _root;
+            depth = _depth;
+        }
+    }
+    */
+    /*Iterative DFS
+    public int maxDepth(TreeNode root) {
+       if(root == null) return 0;
+       Stack<Pair> st = new Stack<>();
+       st.add(new Pair(root,1));
+       int max = 0;
+       while(!st.isEmpty()){
+           Pair node = st.pop();
+           if(node.root!=null){
+               st.add(new Pair(node.root.left,node.depth+1));
+               st.add(new Pair(node.root.right,node.depth+1));
+               max = Math.max(max,node.depth);
+           }
+       }
+       return max;
+    }
+    */
+    
+    /* BFS
+    class Solution {
+    public int maxDepth(TreeNode root) {
+       if(root == null) return 0;
+       Queue<TreeNode> q = new LinkedList<>();
+       q.add(root);
+       int max=0;
+       while(!q.isEmpty()){
+       int qsize = q.size();
+            while(qsize>0){
+             TreeNode node = q.poll();
+            if(node.left!=null) q.add(node.left);
+            if(node.right!=null) q.add(node.right);
+            qsize--;
+            }
+             max++;
+       }
+       return max;
+    }
+}*/
+    /*Recursive DFS*/
 class Solution {
    
     
