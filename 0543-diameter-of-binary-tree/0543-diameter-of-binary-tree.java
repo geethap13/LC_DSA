f54b07e53dde5f7c816c472940c3e35f1132348b
Geetha <104586263+geethap13@users.noreply.github.com>
2023-03-28 20:44:58 +0530

    Time: 13 ms (9.73%), Space: 42 MB (75.49%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,7 +13,33 @@
  *     }
  * }
  */
-
+/*
+class Solution {
+    class Pair{
+        int height,diameter,max;
+        Pair(int _h,int _d,int _m){
+            height = _h;
+            diameter = _h;
+            max = _m;
+        }
+    
+    }
+    public Pair diameterOfBinaryTreeHelper(TreeNode root){
+        if(root == null)  return new Pair(0,0,0);
+        Pair l = diameterOfBinaryTreeHelper(root.left);
+        Pair r = diameterOfBinaryTreeHelper(root.right);
+        int diameter = l.height +r.height;
+        int height = Math.max(l.height,r.height) + 1;
+        int max = Math.max(Math.max(l.max,r.max),diameter);
+        return new Pair(height,diameter,max);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] diameter = new int[1];
+        Pair p = diameterOfBinaryTreeHelper(root);
+         return p.max;
+    }
+}
+*/
  /*
  class Solution {
     int maxi = 0;
