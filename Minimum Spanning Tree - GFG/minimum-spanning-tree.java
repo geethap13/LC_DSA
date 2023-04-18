c798b06963a619fff8a767c57faeff161e02782f
Geetha <104586263+geethap13@users.noreply.github.com>
2023-04-18 17:31:04 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -32,54 +32,52 @@ public class Main{
 
 
 // User function Template for Java
-class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
+class DisjointSet{
     List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
-            rank.add(0);
-            parent.add(i);
-            size.add(1);
-        }
-    }
-
-    public int findUPar(int node) {
-        if (node == parent.get(node)) {
-            return node;
-        }
-        int ulp = findUPar(parent.get(node));
-       // parent.set(node, ulp);
-        return ulp;
-    }
-
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
-        }
-    }
-
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-        } else {
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
-        }
-    }
+     List<Integer> size = new ArrayList<>();
+      List<Integer> rank = new ArrayList<>();
+      DisjointSet(int V){
+          for(int i=0;i<V;i++){
+              parent.add(i);
+              size.add(1);
+              rank.add(0);
+          }
+      }
+      public int findUP(int node){
+          if(node == parent.get(node)) return node;
+          int UP = findUP(parent.get(node));
+          parent.set(node,UP);
+          return UP;
+      }
+      public void UnionBySize(int u,int v){
+          int u_UP = findUP(u);
+          int v_UP = findUP(v);
+          if(u_UP == v_UP) return;
+          if(size.get(u_UP) < size.get(v_UP)){
+              parent.set(u_UP,v_UP);
+              size.set(v_UP,size.get(u_UP)+size.get(v_UP));
+          }
+          else {
+              parent.set(v_UP,u_UP);
+              size.set(u_UP,size.get(u_UP)+size.get(v_UP));
+          }
+          
+      }
+      public void UnionByRank(int u,int v){
+           int u_UP = findUP(u);
+          int v_UP = findUP(v);
+          if(u_UP == v_UP) return;
+          if(rank.get(u_UP) < rank.get(v_UP)){
+              parent.set(u_UP,v_UP);
+          }
+          else if(rank.get(u_UP) > rank.get(v_UP)){
+              parent.set(v_UP,u_UP);
+          }
+          else{
+              parent.set(v_UP,u_UP);
+              rank.set(u_UP,rank.get(u_UP)+rank.get(v_UP));
+          }
+      }
 }
 class Solution{
     static void sortByCol(int[][] edges){
@@ -105,9 +103,9 @@ class Solution{
             int u = edges[i][0];
             int v = edges[i][1];
 
-            if (ds.findUPar(u) != ds.findUPar(v)) {
+            if (ds.findUP(u) != ds.findUP(v)) {
                 mstWt += wt;
-                ds.unionBySize(u, v);
+                ds.UnionBySize(u, v);
             }
         }
 
